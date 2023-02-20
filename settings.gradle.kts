a19dbafd43ba970c3e20abb2c9e806859eb6a2aa
i23098 <ric.almeida@gmail.com>
2023-02-20 18:51:15 +0000

    Add tomcatLib

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 rootProject.name = "Gradle 8.0 Experiment"
 
-include("regularLibA", "regularLibB")
+include("regularLibA", "regularLibB", "tomcatLib")
---
+++ tomcatLib/src/main/java/com/github/i23098/tomcat/T.java
@@ -0,0 +1,15 @@
+package com.github.i23098.tomcat;
+
+import com.github.i23098.a.A;
+
+public class T {
+    A a;
+
+    public T(A a) {
+        this.a = a;
+    }
+
+    public String getString() {
+        return a.getString();
+    }
+}
